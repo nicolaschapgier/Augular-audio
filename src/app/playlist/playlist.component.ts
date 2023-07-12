c79e28ec498b70a4ecd389c492e49acd64d1b0e8
Nicolas Chapgier <nchapgier@hotmail.fr>
2023-07-12 19:24:53 +0200

    manage playlist immediatly play after add song from form

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -17,20 +17,7 @@ export class PlaylistComponent {
   }
 
   deleteSong(index: number): void {
-    console.log('arrEmitFromForm : ', this.arrSongEmitFromForm[index]);
-    console.log('songsToPlay : ', this.arrSongEmitFromForm[index]);
-
     // URL.revokeObjectURL(this.songsToPlay[index]); // empêche la boucle infini si on supprime le morceau en cours de lecture
     this.arrSongEmitFromForm.splice(index, 1);
-  }
-
-  lunchPlaylist(): void {
-    console.log('array de chanson ', this.arrSongEmitFromForm);
-    this.songsToPlay = this.arrSongEmitFromForm;
-    // this.songsToPlay.push(this.arrSongEmitFromForm);
-    // on push un index dans lecteur
-    // -- Observable ? -- //
-    // une fois que la chanson est terminée on push la suivante
-    // si il n'y a pas d'index suivant alors on revient à l'index 1
   }
 }
